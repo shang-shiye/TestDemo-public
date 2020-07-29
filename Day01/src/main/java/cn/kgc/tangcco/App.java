6315d09e68303c89e852b7dba0ad4c3df9d729e1
shang-shiye <67030121+shang-shiye@users.noreply.github.com>
2020-07-29 18:42:34 +0800

    add Day01
    添加Day01

---
+++ Day01/src/main/java/cn/kgc/tangcco/App.java
@@ -0,0 +1,16 @@
+package cn.kgc.tangcco;
+
+import org.graalvm.compiler.api.replacements.Snippet;
+
+/**
+ * Hello world!
+ *
+ */
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
